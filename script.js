e0d3eb6558808c3239dc4e84f63fcca388a0f3a1
Dmitry Maksimetny <maksimetny@gmail.com>
2021-03-08 10:24:29 +0300

    default username for random redirect

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
-const urls = [
-    'https://github.com/maksimetny',
-    'https://vk.com/maksimetny',
-    'https://instagram.com/maksimetny',
-    'https://fb.com/maksimetny',
-    'https://twitter.com/maksimetny',
-    'https://t.me/maksimetny',
-];
-const randomInteger = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
-const rand = randomInteger(0, urls.length - 1);
+(() => {
+    const generate = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    const def = 'maksimetny';
+    const ref = [
+        ['github.com'],
+        ['vk.com'],
+        ['instagram.com'],
+        ['fb.com'],
+        ['twitter.com'],
+        ['t.me'],
+        // ['example.com', 'username'] or ['example.com'] for default username
+    ];
+    const [a, b = def] = ref[generate(0, ref.length - 1)];
 
-window.location.href = urls[rand];
+    window.location.replace(`https://${a}/${b}`);
+})();
